839680b45cf57331a5565761872e3daea4bc1983
Sajjad Bashar <sajjad.bashar@gmail.com>
2019-11-09 16:24:04 -0800

    Initializing CameraIQ code challenge project

---
+++ src/main/java/com/sajjadbashar/cameraIQ/CameraIqApplication.java
@@ -0,0 +1,13 @@
+package com.sajjadbashar.cameraIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameraIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameraIqApplication.class, args);
+	}
+
+}
